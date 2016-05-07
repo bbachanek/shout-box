5906420b861b3d3236e65665a9c9130ac1a07dda
bbachanek <bbachanek@sigma.ug.edu.pl>
2016-05-07 19:34:05 +0200

    shout-box

---
+++ server/main.js
@@ -0,0 +1,12 @@
+
+Meteor.methods({
+  'submitPost': function  ( title , body ) {
+    console.log(title);
+    console.log(body);
+
+    Blogs.insert({
+      title: title,
+      body: body
+    })
+  }
+});
